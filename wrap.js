a9fdb65aee708b92f0ffb78b4c165873cde369af
Minh Nguyen <minh@ellesa.com>
2018-05-08 11:24:03 -0500

    Added wrap logic

--- wrap.js
+++ wrap.js
@@ -1,7 +1,31 @@
 
 
-function wrap (string, cols) {
-
+function wrap (str, col) {
+    let curpos = 0;
+    let expectedVal = (str.length % col) ? Math.floor(str.length/col) + 1 : Math.floor(str.length/col);
+    console.log('Length: '+ str.length +' Expected lines are: '+expectedVal)
+    while(curpos + col < str.length){
+        if(str[curpos + col] === ' '){
+            str[curpos+col] = '\n';
+            curpos = curpos + col + 1;
+        } else {
+            for(let i=curpos+col; i > 0; i--){
+                if(str[i] === ' '){
+                    str[i] = '\n';
+                    curpos = curpos+col - i + 1;
+                }
+            }
+        }
+    }
+    return str;
 }
 
 module.exports = wrap
+
+
+
+// loop thru the string at the str[col] 
+// if a space, then replace 'space' with ' '\n' at str[col]
+// else if str[col] find the next space on the left side, then replace str[space] with '\n'
+
+// then then starting point is now at index = last space position
--- wrap.spec.js
+++ wrap.spec.js
@@ -6,6 +6,22 @@ describe('wrap', function (){
     it('is a function', function(){
       expect(wrap).to.be.a('function')
     });
+    it('test split string', function(){
+      let str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+      let col = 20;
+      let counter = 0;
+      let expectedVal = (str.length % col) ? Math.floor(str.length/col) + 1 : Math.floor(str.length/col);
+
+      res = wrap(str, col);
+      for(let i=0; i<res.length; i++){
+        if( res[i] === '\n')
+          counter++;
+      }
+      expect(counter).to.be(expectedVal)
+    });
+    it('returns a string', function(){
+        expect(wrap('Hello World', 6)).to.be.a('string')
+    });
   });
 });
 
